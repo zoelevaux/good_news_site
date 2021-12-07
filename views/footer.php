8a7a47f8a3496a6ca27bc61e787bce404b6da0e9
Zoé Levaux <zoe.levaux@student.vinci.be>
2021-12-07 17:19:38 +0100

    modify - footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,5 @@
 		<div id="pied">
-			<strong>Excellente journée qu'aujourd'hui le <?php echo $date ?></strong> :: 
+			<strong>Fabuleuse journée qu'aujourd'hui le <?php echo $date ?></strong> ::
 			<?php $time_end = microtime(true);
 				  $time = number_format(($time_end - $time_start)*1000,6);
 				  echo $time; ?>ms pour exécuter le script PHP :: 
